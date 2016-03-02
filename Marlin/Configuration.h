e9a9748249b53e6c72dedee291e6f5bf530fade7
RoaddogLabs <dave@lebowski.local>
2016-03-01 18:39:39 -0800

    config for 1/32 microstep

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -588,13 +588,14 @@ const bool Z_PROBE_ENDSTOP_INVERTING = true; // set to true to invert the logic 
 
 // default settings
 
-//#define DEFAULT_AXIS_STEPS_PER_UNIT   {80,80,4000,98}  // default steps per unit for Bart Pro XL Wilson direct drive extruder
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {80,80,4000,455}  // default steps per unit for Bart Pro XL Gregs Wades geared extruder
+//#define DEFAULT_AXIS_STEPS_PER_UNIT   {80,80,4000,98}  // default steps per unit at 1/16 for Bart Pro XL Wilson direct drive extruder
+//#define DEFAULT_AXIS_STEPS_PER_UNIT   {80,80,4000,455}  // default steps per unit at 1/16 for Bart Pro XL Gregs Wades geared extruder
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {160,160,8000,910}  // default steps per unit at 1/32 for Bart Pro XL Gregs Wades geared extruder
 
-#define DEFAULT_MAX_FEEDRATE          {300, 300, 2, 25}    // (mm/sec)
-#define DEFAULT_MAX_ACCELERATION      {3000,3000,10,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for Skeinforge 40+, for older versions raise them a lot.
+#define DEFAULT_MAX_FEEDRATE          {800, 800, 2, 25}    // (mm/sec)
+#define DEFAULT_MAX_ACCELERATION      {6000,6000,10,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for Skeinforge 40+, for older versions raise them a lot.
 
-#define DEFAULT_ACCELERATION          3000    // X, Y, Z and E acceleration in mm/s^2 for printing moves
+#define DEFAULT_ACCELERATION          6000    // X, Y, Z and E acceleration in mm/s^2 for printing moves
 #define DEFAULT_RETRACT_ACCELERATION  3000    // E acceleration in mm/s^2 for retracts
 #define DEFAULT_TRAVEL_ACCELERATION   3000    // X, Y, Z acceleration in mm/s^2 for travel (non printing) moves
 
